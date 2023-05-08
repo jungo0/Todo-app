2aedab14a5cfe0d99f0b2ab68adef65addeb58ad
jungo <mkk90645@gmail.com>
2023-05-08 13:56:58 +0900

    component생성

--- src/App.js
+++ src/App.js
@@ -4,12 +4,40 @@ import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 import { v4 as uuid } from "uuid";
 // import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import TodoTemplate from "./component/TodoTemplate";
-import TodoNav from "./component/TodoNav";
-import TodoHead from "./component/TodoHead";
-import TodoList from "./component/TodoList";
-import TodoCreate from "./component/TodoCreate";
-import TodoUpdate from "./component/TodoUpdate";
+import TodoTemplate from "./component/todo_template";
+import TodoNav from "./component/todo_nav";
+import TodoHead from "./component/todo_head";
+import TodoList from "./component/todo_list";
+import TodoCreate from "./component/todo_create";
+import TodoUpdate from "./component/todo_update";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  font-family:'Pretendard';
+;
+}
+body {
+	background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+	background-size: 400% 400%;
+	animation: gradient 15s ease infinite;
+	height: 100vh;
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+`;
 
 function App() {
   return (
